97b7ee393cdb3d85f73c493933a5c7747f6ca647
jasonwu123 <jasonwu523215@gmail.com>
2023-05-23 17:33:51 +0800

    创建区块和区块链

---
+++ blockchain.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	prevHash     string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, prevHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.prevHash = prevHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("nonce               %d\n", b.nonce)
+	fmt.Printf("previous_hash       %s\n", b.prevHash)
+	fmt.Printf("timestamp           %d\n", b.timestamp)
+	fmt.Printf("transactions        %s\n", b.transactions)
+}
+
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, prevHash string) *Block {
+	b := NewBlock(nonce, prevHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 60))
+}
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	blockChain := NewBlockchain()
+	blockChain.Print()
+	blockChain.CreateBlock(5, "hash 1")
+	blockChain.Print()
+}
