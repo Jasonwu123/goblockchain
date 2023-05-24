b0c1cf262f1f4b04669996ba6d772ab2d3402982
jasonwu123 <jasonwu523215@gmail.com>
2023-05-24 18:01:13 +0800

    增加钱包和交易验证功能

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"goblockchain/block"
+	"goblockchain/wallet"
 	"log"
 )
 
@@ -10,24 +12,44 @@ func init() {
 }
 
 func main() {
-	myBlockchainAddress := "my_blockchain_address"
+	/*
+		myBlockchainAddress := "my_blockchain_address"
 
-	blockChain := NewBlockchain(myBlockchainAddress)
-	blockChain.Print()
+		blockChain := block.NewBlockchain(myBlockchainAddress)
+		blockChain.Print()
 
-	blockChain.AddTransaction("A", "B", 1.0)
-	blockChain.Mining()
-	blockChain.Print()
+		blockChain.AddTransaction("A", "B", 1.0)
+		blockChain.Mining()
+		blockChain.Print()
 
-	blockChain.AddTransaction("C", "D", 2.0)
-	blockChain.AddTransaction("X", "Y", 3.0)
-	blockChain.Mining()
-	blockChain.Print()
+		blockChain.AddTransaction("C", "D", 2.0)
+		blockChain.AddTransaction("X", "Y", 3.0)
+		blockChain.Mining()
+		blockChain.Print()
 
-	fmt.Printf("my %.1f\n", blockChain.CalculdateTotalAmount("my_blockchain_address"))
-	fmt.Printf("A %.1f\n", blockChain.CalculdateTotalAmount("A"))
-	fmt.Printf("B %.1f\n", blockChain.CalculdateTotalAmount("B"))
-	fmt.Printf("C %.1f\n", blockChain.CalculdateTotalAmount("C"))
-	fmt.Printf("D %.1f\n", blockChain.CalculdateTotalAmount("D"))
+		fmt.Printf("my %.1f\n", blockChain.CalculdateTotalAmount("my_blockchain_address"))
+		fmt.Printf("A %.1f\n", blockChain.CalculdateTotalAmount("A"))
+		fmt.Printf("B %.1f\n", blockChain.CalculdateTotalAmount("B"))
+		fmt.Printf("C %.1f\n", blockChain.CalculdateTotalAmount("C"))
+		fmt.Printf("D %.1f\n", blockChain.CalculdateTotalAmount("D"))
+	*/
+
+	wm := wallet.NewWallet()
+	wa := wallet.NewWallet()
+	wb := wallet.NewWallet()
+	t := wallet.NewTransaction(wa.PrivateKey(), wa.PublicKey(), wa.BlockchainAddress(), wb.BlockchainAddress(), 1.0)
+
+	bc := block.NewBlockchain(wm.BlockchainAddress())
+
+	isAdded := bc.AddTransaction(wa.BlockchainAddress(), wb.BlockchainAddress(), 1.0, wa.PublicKey(), t.GenerateSignature())
+
+	fmt.Println("Added? ", isAdded)
+
+	bc.Mining()
+	bc.Print()
+
+	fmt.Printf("A %.1f\n", bc.CalculdateTotalAmount(wa.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", bc.CalculdateTotalAmount(wb.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", bc.CalculdateTotalAmount(wm.BlockchainAddress()))
 
 }
